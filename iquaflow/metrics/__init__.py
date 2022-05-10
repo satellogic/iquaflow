f5883ac645ad29d127a0d75ae1aeb772405d79f0
Katalin Takats <katalin.takats@satellogic.com>
2022-05-10 12:55:23 +0200

    code formatting

--- iquaflow/metrics/__init__.py
+++ iquaflow/metrics/__init__.py
@@ -1,10 +1,10 @@
 from .base_metric import Metric
 from .detection_metrics import BBDetectionMetrics
-from .rer_metric import RERMetric
+from .sharpness_metric import SharpnessMetric
 from .snr_metric import (  # noqa: F401
     SNRMetric,
     snr_function_from_array,
     snr_function_from_fn,
 )
 
-__all__ = ["Metric", "BBDetectionMetrics", "RERMetric", "SNRMetric"]
+__all__ = ["Metric", "BBDetectionMetrics", "SharpnessMetric", "SNRMetric"]
