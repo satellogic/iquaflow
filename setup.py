94825c537cf71f8a538f5a2d7b5c7285e2f8258e
jmarin <javier.marin@satellogic.com>
2022-10-18 15:27:11 +0200

    setup changes

--- setup.py
+++ setup.py
@@ -2,8 +2,19 @@ from setuptools import setup
 import versioneer
 
 setup(
+    name='iquaflow',
+    author='Pau Gallés, Katalin Takáts, Emilio Tylson, David Berga, Miguel Hernández-Cabronero, Luciano Pega, Laura Riordan-Chen, Clara Garcia, Guillermo Becker, David Vilaseca, Javier Marín',
+    description='Image quality framework',
+    long_description=open('README.md', encoding='utf-8').read(),
     version=versioneer.get_version(),
+    maintainer_email='iquaflow@satellogic.com',
+    license="MIT",
     cmdclass=versioneer.get_cmdclass(),
+    url='https://github.com/satellogic/iquaflow/',
+    download_url=(
+        'https://github.com/satellogic/iquaflow/tags/v' +
+        versioneer.get_version()
+    ),
     package_data={
         'iquaflow': [
             'quality_metrics/cfgs_example/*.cfg',
