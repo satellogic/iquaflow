78cb5a114d5ecc29a6443ba7beecd15220785c0c
jmarin <javier.marin@satellogic.com>
2022-10-18 15:55:43 +0200

    Long description visualization on Pypi

--- setup.py
+++ setup.py
@@ -24,5 +24,6 @@ setup(
             'datasets/dataset_labels/SNR/*.json'
         ]
     },
-    include_package_data=True
+    long_description_content_type='text/markdown',
+    include_package_data=True,
 )
